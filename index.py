4c3716e09c0a446f3f91ba432d7c69ac5407db6f
FahadAliHashmi2011 <faiezali2006@gmail>
2025-11-22 15:29:04 +0000

    hy

--- index.py
+++ index.py
@@ -13,3 +13,19 @@ modelX = Vehicle(240, 18)
 
 print("Model Max Speed:", modelX.max_speed)
 print("Model Mileage:", modelX.mileage)
+#-------------========------------------------------------------------------------------======================================================
+class Parrot:
+    species = "bird"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+blu = Parrot("Blu", 10)
+woo = Parrot("Woo", 15)
+
+print("Blu is a {}".format(blu.species))
+print("Woo is also a {}".format(woo.species))
+
+print("{} is {} years old".format(blu.name, blu.age))
+print("{} is {} years old".format(woo.name, woo.age))
